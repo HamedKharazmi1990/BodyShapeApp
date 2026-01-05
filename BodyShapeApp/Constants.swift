7f8ff0b2596f34c6419035aeb91beb0232a84a18
hamed kharazmi <hamed.kharazmi@bandpey.com>
2026-01-05 23:06:30 +0100

    Added constant file for colors

---
+++ BodyShapeApp/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  BodyShapeApp
+//
+//  Created by Hamed Kharazmi on 05.01.26.
+//
+
+import Foundation
+import SwiftUI
+
+let lightGreen = Color("lightGreen")
+let lightPurple = Color("lightPurple")
