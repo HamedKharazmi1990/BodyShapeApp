b5a6ee18776c2088cdfc7e1b1f03b0b93439e8e8
hamed kharazmi <hamed.kharazmi@bandpey.com>
2026-01-05 22:57:30 +0100

    Initial Commit

---
+++ BodyShapeApp/BodyShapeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BodyShapeAppApp.swift
+//  BodyShapeApp
+//
+//  Created by Hamed Kharazmi on 05.01.26.
+//
+
+import SwiftUI
+
+@main
+struct BodyShapeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BodyShapeApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BodyShapeApp
+//
+//  Created by Hamed Kharazmi on 05.01.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
