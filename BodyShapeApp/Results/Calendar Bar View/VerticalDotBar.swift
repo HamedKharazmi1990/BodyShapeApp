ae8e01ccbea1914b87802b2b1349ffd402f55dd7
hamed kharazmi <hamed.kharazmi@bandpey.com>
2026-01-08 14:36:55 +0100

    Added vertical dot bar to activity view

--- BodyShapeApp/Results/Calendar Bar View/VerticalDotBar.swift
+++ BodyShapeApp/Results/Calendar Bar View/VerticalDotBar.swift
@@ -11,8 +11,6 @@ struct VerticalDotBar: View {
     let percent: CGFloat
     let isSelected: Bool
     let height: CGFloat
-    let day: String
-    let amount: Int
     let offsetY: CGFloat
     let selectedWeight: Int
     let selectedWeightUnits: String = "lb"
@@ -46,6 +44,7 @@ struct VerticalDotBar: View {
                                 height: circleDim + delta
                             )
                             .offset(y: -(height - circleDim) * percent)
+                            .offset(y: -offsetY)
                     }
                     
                     Circle()
@@ -54,6 +53,7 @@ struct VerticalDotBar: View {
                             height: circleDim
                         )
                         .offset(y: -(height - circleDim) * percent)
+                        .offset(y: -offsetY)
                 }
             }
         }
@@ -66,8 +66,6 @@ struct VerticalDotBar: View {
             percent: 0.6,
             isSelected: false,
             height: 100,
-            day: "Mon",
-            amount: 7,
             offsetY: -5,
             selectedWeight: 12
         )
@@ -76,8 +74,6 @@ struct VerticalDotBar: View {
             percent: 1,
             isSelected: true,
             height: 100,
-            day: "Tue",
-            amount: 7,
             offsetY: -5,
             selectedWeight: 12
         )
