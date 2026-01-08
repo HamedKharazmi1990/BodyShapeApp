0c2c782e090314b31a2ec64b7b309b3f4f8003b2
hamed kharazmi <hamed.kharazmi@bandpey.com>
2026-01-08 17:06:07 +0100

    Main activity view is completed

--- BodyShapeApp/Results/ResultView.swift
+++ BodyShapeApp/Results/ResultView.swift
@@ -49,7 +49,30 @@ struct ResultView: View {
                 )
             }
             
-            MainActivitiesView()
+            MainActivitiesView(
+                activities: [
+                    .init(
+                        dim: 150,
+                        activityName: "Running",
+                        activityImage: "figure.run"
+                    ),
+                    .init(
+                        dim: 150,
+                        activityName: "Biking",
+                        activityImage: "figure.outdoor.cycle"
+                    ),
+                    .init(
+                        dim: 150,
+                        activityName: "Climbing",
+                        activityImage: "figure.climbing"
+                    ),
+                    .init(
+                        dim: 150,
+                        activityName: "Skating",
+                        activityImage: "figure.skating"
+                    )
+                ]
+            )
             
             HStack {
                 Text("Your plan for January, 2026")
